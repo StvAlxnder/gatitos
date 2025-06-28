43688648702f56ad5331186d3e68ecc5feb5bf8f
StvAlxnder <smamanie@unjbg.edu.pe>
2025-06-27 23:02:43 -0500

    Agrega gato.cpp

---
+++ gato.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string miau;
+    system("cls");
+    cout<<"Programa gato";
+    cout<<"Escriba miau: "; cin>>miau;
+    if(miau=="miau"){
+        cout<<"/ᐠ - ˕ -マ";
+    }
+    else{
+        cout<<"Error: no miau";
+    }
+    return 0;
+}
