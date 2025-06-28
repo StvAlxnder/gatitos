aae2cbe6e6e89ef2b4e3596674197a08a2f19ae1
StvAlxnder <smamanie@unjbg.edu.pe>
2025-06-27 23:06:19 -0500

    Miau

--- gato.cpp
+++ gato.cpp
@@ -4,13 +4,19 @@ using namespace std;
 int main(){
     string miau;
     system("cls");
-    cout<<"Programa gato";
+    cout<<"Programa gato"<<endl;
     cout<<"Escriba miau: "; cin>>miau;
     if(miau=="miau"){
-        cout<<"/ᐠ - ˕ -マ";
+        cout << R"( 
+    /\_/\  
+    ( o.o ) 
+     > ^ < 
+    )";
+    cout<<endl<<endl;
     }
     else{
         cout<<"Error: no miau";
     }
+    system("pause");
     return 0;
 }
